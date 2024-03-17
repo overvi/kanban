775c4d0f53d3de93c273f7fd0f59784cf8702f15
Ali <aliwebdevf@gmail.com>
2024-03-17 16:05:36 +0330

    add copy right

--- app/components/Nav/Heading.tsx
+++ app/components/Nav/Heading.tsx
@@ -2,7 +2,7 @@
 
 import { useBearStore } from "@/app/zustand/useCurrentBoard";
 import { Board } from "@prisma/client";
-import { Box, Flex, Heading as Head } from "@radix-ui/themes";
+import { Box, Flex, Heading as Head, Text } from "@radix-ui/themes";
 import { NavBarModal } from "../Modals/Modals";
 
 const Heading = ({ boards }: { boards: Board[] }) => {
@@ -15,6 +15,10 @@ const Heading = ({ boards }: { boards: Board[] }) => {
           {title?.title || "Nothing Here"}
         </Head>
       </Box>
+      <Text className="font-bold px-8">
+        &copy; 2024 Ali028 All rights reserved.
+      </Text>
+
       <NavBarModal boards={boards} />
     </Flex>
   );
